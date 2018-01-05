0b3cc1c5996eae77456fc06c4552827d2390d72c
Alexei Moisseev <alexmois@ya.ru>
2018-01-05 17:26:51 +0300

    Binary Search Tree

---
+++ javascript/binary-search-tree/binary-search-tree.js
@@ -0,0 +1,75 @@
+class BinarySearchTree {
+
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+
+    insert(val) {
+        if (val < this.val) {
+            if (!this.left) {
+                this.left = new BinarySearchTree(val);
+            } else {
+                this.left.insert(val);
+            }
+        } else {
+            if (!this.right) {
+                this.right = new BinarySearchTree(val);
+            } else {
+                this.right.insert(val);
+            }
+        }
+        return this;
+    }
+
+    find(val) {
+        if (val === this.val) {
+            return this;
+        }
+        if (val < this.val) {
+            if (!this.left) {
+                return null;
+            }
+            return this.left.find(val);
+        } else {
+            if (!this.right) {
+                return null;
+            }
+            return this.right.find(val);
+        }
+    }
+
+    traverse() {
+        return [
+            ...(this.left ? this.left.traverse() : []),
+            this.val,
+            ...(this.right ? this.right.traverse() : []),
+        ];
+    }
+}
+
+
+module.exports = BinarySearchTree;
+
+/*
+
+const root = new BinarySearchTree(5);
+
+root.insert(7)
+    .insert(8)
+    .insert(9)
+    .insert(10)
+    .insert(6);
+
+root.insert(4)
+    .insert(2)
+    .insert(1)
+    .insert(3);
+
+console.log(root);
+
+console.log(root.find(8));
+console.log(root.find(12));
+console.log(root.traverse());
+
+*/
---
+++ javascript/binary-search-tree/binary-search-tree.test.js
@@ -0,0 +1,31 @@
+const BinarySearchTree = require('./binary-search-tree');
+const root = new BinarySearchTree(5);
+
+test('BinarySearchTree can insert', () => {
+    root.insert(3);
+    expect(root.left.val).toBe(3);
+});
+
+
+test('BinarySearchTree can find existing', () => {
+    const three = root.find(3);
+    expect(three).not.toBe(null);
+
+});
+
+test('BinarySearchTree can find null for not existing', () => {
+    const nil = root.find(2);
+    expect(nil).toBe(null);
+})
+
+test('BinarySearchTree can traverse', () => {
+    const oneToSix = new BinarySearchTree(1)
+        .insert(5)
+        .insert(2)
+        .insert(4)
+        .insert(3)
+        .insert(6);
+    const arr = oneToSix.traverse();
+
+    expect(arr).toEqual([1, 2, 3, 4, 5, 6]);
+});
